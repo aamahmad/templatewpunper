49d3c2ddf07f24a1bbd9790d0b45c7673982f495
aam <am.mj03@gmmail.coom>
2019-03-09 10:51:09 +0700

    hilangkan gallery dan program

--- index.php
+++ index.php
@@ -53,69 +53,7 @@
     </div>
 
     <!--program -->
-    <div class="row" style="margin-bottom: 40px;">
-      <div class="container">
-        <div class="col-md-3">
-          <div class="tw-service-box style-2 left-service">
-
-            <div class="tw-service-icon">
-              <i class="fa fa-desktop" style="border-style: solid;font-style:normal;text-align:center;font-size:32px;line-height:34px;padding:18px;color:#ffffff;background-color:#065139;border-color:#065139;border-width:2px;border-radius:4px;-webkit-border-radius:4px;-moz-border-radius:4px;"></i>
-            </div>
-
-            <div class="tw-service-content desc_unstyle" style="margin-left:92px;">
-              <h3>SiAkad</h3><p>Aplikasi pengelolaan data akademik Universitas Perjuangan.</p>
-              <a href="http://appunper.xyz" target="_blank">Kunjungi halaman</a>
-            </div>
-
-          </div>
-        </div>
-
-        <div class="col-md-3">
-          <div class="tw-service-box style-2 left-service">
-
-            <div class="tw-service-icon">
-              <i class="fa fa-file-pdf-o" style="border-style: solid;font-style:normal;text-align:center;font-size:32px;line-height:34px;padding:18px;color:#ffffff;background-color:#065139;border-color:#065139;border-width:2px;border-radius:4px;-webkit-border-radius:4px;-moz-border-radius:4px;"></i>
-            </div>
-
-            <div class="tw-service-content desc_unstyle" style="margin-left:92px;">
-              <h3>e - Journal</h3><p>Aplikasi publikasi hasil penelitian Mahasiswa dan Dosen.</p>
-              <a href="http://appunper.xyz" target="_blank">Kunjungi halaman</a>
-            </div>
-
-          </div>
-        </div>
-
-        <div class="col-md-3">
-          <div class="tw-service-box style-2 left-service">
-
-            <div class="tw-service-icon">
-              <i class="fa fa-users" style="border-style: solid;font-style:normal;text-align:center;font-size:32px;line-height:34px;padding:18px;color:#ffffff;background-color:#065139;border-color:#065139;border-width:2px;border-radius:4px;-webkit-border-radius:4px;-moz-border-radius:4px;"></i>
-            </div>
-
-            <div class="tw-service-content desc_unstyle" style="margin-left:92px;">
-              <h3>Sisfo KKN</h3><p>Aplikasi Sistem Informasi KKn Universitas Perjuangan</p>
-              <a href="http://appunper.xyz" target="_blank">Kunjungi halaman</a>
-            </div>
-
-          </div>
-        </div>
-
-        <div class="col-md-3">
-          <div class="tw-service-box style-2 left-service">
-
-            <div class="tw-service-icon">
-              <i class="fa fa-graduation-cap" style="border-style: solid;font-style:normal;text-align:center;font-size:32px;line-height:34px;padding:18px;color:#ffffff;background-color:#065139;border-color:#065139;border-width:2px;border-radius:4px;-webkit-border-radius:4px;-moz-border-radius:4px;"></i>
-            </div>
-
-            <div class="tw-service-content desc_unstyle" style="margin-left:92px;">
-              <h3>Alumni Tracer</h3><p>Aplikasi penelusuran alumni Universitas Perjuangan</p>
-              <a href="http://appunper.xyz" target="_blank">Kunjungi halaman</a>
-            </div>
-
-          </div>
-        </div>
-      </div>
-    </div>
+    
 
 <!--  <hr class="style-two"> -->
 
@@ -288,43 +226,6 @@
         		</div>
         	</div>
 
-        <div class="col-md-12">
-          <div class="widget-item">
-            <h3>Gallery
-            <a href="http://appunper.xyz/gallery/" class="btn btn-primary btn-xs pull-right">View more gallery</a>
-            </h3>
-            <section class="gallery no-gutter">
-              <div class="row">
-                <?php
-                // loop tampil produk
-                $the_query = new WP_Query(array(
-                  'post_status' => 'publish',
-                  'posts_per_page' => 10,
-                  'post_type' => 'gallery',
-                ));
-                ?>
-
-                <?php if ($the_query->have_posts()) : ?>
-                  <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
-                    <?php
-                     /* Include the Post-Format-specific template for the content.
-                      * If you want to overload this in a child theme then include a file
-                      * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-                      */
-                      get_template_part( 'content-gallery', get_post_format() );
-                    ?>
-                    <?php endwhile; ?>
-                    <?php wp_reset_postdata(); ?>
-                <?php endif; ?>
-
-              </div>
-              <div class="col-xs-12">
-
-              </div>
-            </section>
-          </div>
-        </div>
-        <!--end-->
   </div>
   </div>
 
@@ -410,7 +311,7 @@
 -->
 
 
-<section class="home_section" id="section_client" style="margin-bottom: 10px;">
+<section class="home_section" id="section_client">
   <div class="bg_client clearfix">
     <div class="ak-container">
       <div class="client_wrap">
@@ -423,7 +324,7 @@
             </div>
         </div>
 
-        <div class="client_cat_loop owl-carousel wow fadeInUp owl-loaded owl-drag" style="visibility: visible; animation-name: fadeInUp;">
+        <div class="client_cat_loop owl-carousel wow fadeInUp owl-loaded owl-drag" style="margin-bottom: 15px; visibility: visible; animation-name: fadeInUp;">
             <div class="owl-stage-outer">
               <div class="owl-stage" style="transform: translate3d(0px, 0px, 0px); transition: all 0s ease 0s; width: 991px;">
                   <div class="owl-item active" style="width: 131.571px; margin-right: 10px;">
@@ -446,17 +347,19 @@
 
                   <div class="owl-item active" style="width: 131.571px; margin-right: 10px;"><div class="client_image">
                                             <img src="https://demo.accesspressthemes.com/enlighten/wp-content/uploads/2016/05/4.jpg" alt="Client 6">
-                                        </div></div><div class="owl-item active" style="width: 131.571px; margin-right: 10px;"><div class="client_image">
+                                        </div>
+                  </div>
+
+                  <div class="owl-item active" style="width: 131.571px; margin-right: 10px;"><div class="client_image">
                                             <img src="https://demo.accesspressthemes.com/enlighten/wp-content/uploads/2016/05/5.jpg" alt="Client 5">
                                         </div></div><div class="owl-item active" style="width: 131.571px; margin-right: 10px;"><div class="client_image">
                                             <img src="https://demo.accesspressthemes.com/enlighten/wp-content/uploads/2016/05/l3.jpg" alt="Client 4">
                                         </div></div><div class="owl-item active" style="width: 131.571px; margin-right: 10px;"><div class="client_image">
                                             <img src="https://demo.accesspressthemes.com/enlighten/wp-content/uploads/2016/05/2-100x100.jpg" alt="Client 3">
-                                        </div></div><div class="owl-item active" style="width: 131.571px; margin-right: 10px;"><div class="client_image">
-                                            <img src="https://demo.accesspressthemes.com/enlighten/wp-content/uploads/2016/05/7-100x94.jpg" alt="Client 2">
-                                        </div></div><div class="owl-item active" style="width: 131.571px; margin-right: 10px;"><div class="client_image">
-                                            <img src="https://demo.accesspressthemes.com/enlighten/wp-content/uploads/2016/05/6.jpg" alt="Client 1">
-                                        </div></div></div></div><div class="owl-nav disabled"><button type="button" role="presentation" class="owl-prev disabled"><i class="fa fa-angle-left" aria-hidden="true"></i></button><button type="button" role="presentation" class="owl-next disabled"><i class="fa fa-angle-right" aria-hidden="true"></i></button></div><div class="owl-dots disabled"><button role="button" class="owl-dot active"><span></span></button></div>
+                                        </div></div>
+
+              <div>
+            </div>
           </div>
       </div>
     </div>
