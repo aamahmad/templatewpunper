c8500d7d9686ee9615e798f2a4b913476af9a2dd
Aamwebpro <am.mj03@gmail.com>
2019-03-11 20:54:00 +0700

    related konten

--- sidebar.php
+++ sidebar.php
@@ -1,31 +1,66 @@
 <div class="sidebar">
+
 	<section class="widget_recent_entries widgets">
-		<h3 class="widget_title">Berita Terkini</h3>
+		<h3 class="widget_title">Related
+			<?php
+				$categories = get_the_category();
+				if ( ! empty( $categories ) ) {
+					echo '' . esc_html( $categories[0]->name ) . '';
+				}
+			?>
+		</h3>
+		
 		<nav>
 			<ul>
+
 				<?php
-				$args = array( 'numberposts' => '10');
-				$recent_posts = wp_get_recent_posts( $args );
-				foreach( $recent_posts as $recent ){ ?>
+				// Default arguments
+				$args = array(
+					'posts_per_page' => 15, // How many items to display
+					'post__not_in'   => array( get_the_ID() ), // Exclude current post
+					'no_found_rows'  => true, // We don't ned pagination so this speeds up the query
+					'orderby' => 'DESC',
+				);
 
-					<li class="normal">
-						<a style="font-weight: 600;" href="<?php echo get_permalink($recent["ID"]); ?>"><?php echo $recent["post_title"];?></a>
-						<span class="date"><?php echo date('j F Y', strtotime($recent['post_date'])); ?> | oleh : <?php the_author(); ?></span>
-					</li>
+				// Check for current post category and add tax_query to the query arguments
+				$cats = wp_get_post_terms( get_the_ID(), 'category' );
+				$cats_ids = array();
+				foreach( $cats as $wpex_related_cat ) {
+					$cats_ids[] = $wpex_related_cat->term_id;
+				}
+				if ( ! empty( $cats_ids ) ) {
+					$args['category__in'] = $cats_ids;
+				}
 
-				<?php }
-				wp_reset_query();
-				?>			
-			</ul>	
+				// Query posts
+				$wpex_query = new wp_query( $args );
+
+
+				// Loop through posts
+				foreach( $wpex_query->posts as $post ) : setup_postdata( $post ); ?>
+
+				<li class="normal">
+					<a style="font-weight: 600;" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+          <span class="date"><?php the_date('j F Y', '', ''); ?> | oleh : <?php the_author(); ?></span>
+				</li>
+
+				<?php
+				// End loop
+				endforeach;
+
+				// Reset post data
+				wp_reset_postdata(); ?>
+
+			</ul>
 		</nav>
 	</section>
-                      
+
 	<section class="widget_categories widgets">
 		<h3 class="widget_title">Kategori</h3>
 		<nav>
 			<ul>
 				<?php
-                $sub_categorys = get_categories( array( 
+                $sub_categorys = get_categories( array(
                 	'hide_empty' 		=> false,
                     'orderby'            => 'id',
                     'show_count'         => __( 'No categories' ),
@@ -33,8 +68,8 @@
                     'title_li'           => false,
                     'show_option_none'   => false,
                     'child_of'           => $id_term
-                )); 
-                
+                ));
+
                 foreach ( $sub_categorys as $subcategory ) { ?>
                     <li class="cat-item">
                         <a class="listkategori<?php if ($thisCat->term_id == $subcategory->term_id) { ?> aktif<?php } ?>" href="<?php echo esc_url( get_category_link( $subcategory->term_id ) );?>" >
@@ -48,5 +83,6 @@
 			</ul>
 		</nav>
 	</section>
-                        
+
+
 </div>
